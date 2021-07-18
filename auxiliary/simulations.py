3c8e921be9885098b40c1a285577648a744fb025
pcschreiber1 <pcschreiber1@gmail.com>
2021-07-18 14:19:47 +0200

    added regression tables 2,3,5,6

--- auxiliary/simulations.py
+++ auxiliary/simulations.py
@@ -27,7 +27,7 @@ def simulate_SLX_sample(num_obs,
 
     Returns:
         Returns a dataframe with the observables (Y, X, D) as well as
-        the unobservables (Y_1, Y_0).
+        the unobservables (Y_1, Y_0) and the weight matrix w.
     """
 
     # Initialize empty data frame
@@ -71,7 +71,7 @@ def simulate_SDM_sample(num_obs,
                             beta = 0.9,
                             gamma = 0.25,
                             rho = 0.05):
-    """Simulate spatial sample with only spillover from treatment variable
+    """Simulate spatial sample with spillover from the treatment and the outcome variable
         "Y = WY+ WD + X"
 
     Args:
@@ -80,7 +80,7 @@ def simulate_SDM_sample(num_obs,
 
     Returns:
         Returns a dataframe with the observables (Y, X, D) as well as
-        the unobservables (Y_1, Y_0).
+        the unobservables (Y_1, Y_0)and the weight matrix w.
     """
 
     # Initialize empty data frame
@@ -96,9 +96,9 @@ def simulate_SDM_sample(num_obs,
             x.shape=(num_obs,1)
             y.shape=(num_obs,1)
             data=np.hstack([x,y])
-    elif num_obs == 10000:
+    elif num_obs == 2500:
             # generate grid for weight matrix
-            x,y=np.indices((100, 100))
+            x,y=np.indices((50, 50))
             x.shape=(num_obs,1)
             y.shape=(num_obs,1)
             data=np.hstack([x,y])
@@ -111,6 +111,74 @@ def simulate_SDM_sample(num_obs,
     # calculate spillovers
     df["WD"] = np.dot(w.full()[0], df["D"].to_numpy())#not standardized
     
+    # outcomes
+    df["Y"] = beta*df["X"] + gamma*df["WD"] + gamma*df["D"] #add D since W is sparse
+    df["Y_1"] = beta*df["X"] + gamma*df["WD"] + gamma
+    df["Y_0"] = beta*df["X"] + gamma*df["WD"]
+    
+    df["Y_no_spill"] = df["Y"]
+    df["Y_1_no_spill"] = df["Y_1"]
+    df["Y_0_no_spill"] = df["Y_0"]
+    
+    # iterate to generate general equilibrium effect (already settles after 5 times for small rho)
+    for i in range(0,10):
+        df["WY"] = np.dot(w.full()[0], df["Y"].to_numpy())#not standardized
+        df["Y"] = beta*df["X"] + gamma*df["WD"] + gamma*df["D"] + rho* df["WY"]   
+        df["Y_1"] = beta*df["X"] + gamma*df["WD"] + gamma + rho* df["WY"]   
+        df["Y_0"] = beta*df["X"] + gamma*df["WD"] + rho* df["WY"]   
+    
+    return df, w
+
+#backdoor
+def simulate_backdoor_sample(num_obs,
+                            knn = 10,
+                            beta = 0.9,
+                            gamma = 0.25,
+                            rho = 0.05):
+    """Simulate spatial sample with spillover from treatment and outcome variable
+        "Y = WY+ WD + X" and "D = WD"
+
+    Args:
+        num_obs: An integer that specifies the number of individuals
+            to sample.
+
+    Returns:
+        Returns a dataframe with the observables (Y, X, D) as well as
+        the unobservables (Y_1, Y_0) and the weight matrix w.
+    """
+
+    # Initialize empty data frame
+    columns = ["Y", "Y_1", "Y_0", "D", "X", "WD"]
+    df = pd.DataFrame(columns=columns, index=range(num_obs))
+
+    df["D"] = np.random.randint(2, size=num_obs) #binary treatment
+    df["X"] = np.random.normal(size=num_obs)
+    
+    if num_obs == 100:
+            # generate grid for weight matrix
+            x,y=np.indices((10, 10))
+            x.shape=(num_obs,1)
+            y.shape=(num_obs,1)
+            data=np.hstack([x,y])
+    elif num_obs == 2500:
+            # generate grid for weight matrix
+            x,y=np.indices((50, 50))
+            x.shape=(num_obs,1)
+            y.shape=(num_obs,1)
+            data=np.hstack([x,y])
+    else:
+            raise AssertionError # needs to be 100 or 10.000 for constructing weight matrix
+
+    # weight matrix
+    w = lp.weights.KNN(data, k = knn)
+    
+    # calculate spillovers
+    df["WD"] = np.dot(w.full()[0], df["D"].to_numpy())/knn #standardized
+
+    # whether you are treated is function of other's treatment
+    df.loc[(df.WD > 0.5), 'D']=1
+    df.loc[(df.WD <= 0.5), 'D']=0
+
     # outcomes
     df["Y"] = beta*df["X"] + gamma*df["WD"] + gamma*df["D"] #add D since W is sparse
     df["Y_1"] = beta*df["X"] + gamma*df["WD"] + gamma
--- auxiliary/tables.py
+++ auxiliary/tables.py
@@ -13,16 +13,16 @@ from auxiliary.data_import import *
 
 pd.options.display.float_format = "{:,.2f}".format
 
-# get reg table
-def get_table_2(regressors, specification, data):
+# get reg table regiondata
+def get_table_regiondata(regressors, specification, data):
     """
-    Generates the regression table 2
+    Can generate the regression table 2,3 and 4
     Inputs:
         - regressors: array of column names
         - specification: dictionary with column names
         - data: data frame (regiondata)
         
-    Returns: container (pandas data frame)
+    Returns: container (pandas data frame with regression results)
     """
     ## get list of regressors
     #regressors = []
@@ -64,5 +64,164 @@ def get_table_2(regressors, specification, data):
             [specification.keys(),
             ['Urbanization rate', 'Std.err', 'P-Value',]]
             )
+    container = container.style.format('{:.2f}',na_rep='')
+
+    return container
+
+# get reg table countrydata
+def get_table_countrydata(regressors, specification, data):
+    """
+    Can generate the regression table 5
+    Inputs:
+        - regressors: array of column names
+        - specification: dictionary with column names
+        - data: data frame (regiondata)
+        
+    Returns: container (pandas data frame with regression results)
+    """
+    ## get list of regressors
+    #regressors = []
+    #for i in specification.keys():
+    #    regressors.extend(specification[i])
+    #regressors = list(set(regressors))
+
+
+    container = pd.DataFrame()
+
+    container['regressors'] = regressors
+    container = container.set_index('regressors')
+
+    for key in specification.keys():
+        if key == "5.5 - Growth of capital city": #one regression on change in primacy
+            table = pd.DataFrame({'Growth of capital city': [], 'Std.err': [], 'P-Value': [],})
+
+            table['regressors'] = regressors
+            table = table.set_index('regressors')
+
+            formula = "ADprimwidefrac ~"
+            for regressors in specification[key]:
+                formula = formula + f" + {regressors}"
+
+
+            result = smf.ols(
+                formula = formula, data=data
+                ).fit(cov_type='HC1')#.fit(
+                #cov_type='cluster',cov_kwds={'groups': data['afruid']},use_t=True
+                #)
+            for coef in specification[key]:
+                outputs = [result.params[coef], result.bse[coef], result.pvalues[coef]]
+                table.loc[coef] = outputs            
+        else: #remaining regressions on change in urban growth
+            table = pd.DataFrame({'Urbanization rate': [], 'Std.err': [], 'P-Value': [],})
+
+            table['regressors'] = regressors
+            table = table.set_index('regressors')
+
+            formula = "ADurbfrac ~"
+            for regressors in specification[key]:
+                formula = formula + f" +{regressors}"
+
+
+            #c(var) for fixed effect - "-1" for dropping intercept
+            result = smf.ols(
+                formula = formula, data=data
+                ).fit(cov_type='HC1')#.fit(
+                #cov_type='cluster',cov_kwds={'groups': data['afruid']},use_t=True
+                #)
+            for coef in specification[key]:
+                outputs = [result.params[coef], result.bse[coef], result.pvalues[coef]]
+                table.loc[coef] = outputs
+
+        
+        container = pd.concat([container, table], axis=1)
+
+
     
+    container.columns = pd.MultiIndex.from_product(
+            [specification.keys(),
+            ['Urbanization rate', 'Std.err', 'P-Value',]]
+            )
+    container = container.style.format('{:.2f}',na_rep='')
+
+    return container
+
+# get reg table citydata
+def get_table_citydata(regressors, specification, data):
+    """
+    Can generate the regression tables 6, ...
+    Inputs:
+        - regressors: array of column names
+        - specification: dictionary with column names
+        - data: data frame (regiondata)
+        
+    Returns: container (pandas data frame with regression results)
+    """
+    container = pd.DataFrame()
+
+    container['regressors'] = regressors
+    container = container.set_index('regressors')
+
+    for key in specification.keys():
+        # if condition not used currently
+        if key == "5.5 - Growth of capital city": #one regression on change in primacy
+            table = pd.DataFrame({'Growth of capital city': [], 'Std.err': [], 'P-Value': [],})
+
+            table['regressors'] = regressors
+            table = table.set_index('regressors')
+
+            formula = "dlnl1 ~"
+            for regressors in specification[key]:
+                formula = formula + f" + {regressors}"
+
+
+            result = smf.ols(
+                formula = formula, data=data
+                ).fit(
+                cov_type='cluster',cov_kwds={'groups': data['agidison']},use_t=True
+                )
+            for coef in specification[key]:
+                outputs = [result.params[coef], result.bse[coef], result.pvalues[coef]]
+                table.loc[coef] = outputs            
+        else: #remaining regressions on change city output
+            table = pd.DataFrame({'City output': [], 'Std.err': [], 'P-Value': [],})
+
+            table['regressors'] = regressors
+            table = table.set_index('regressors')
+
+            formula = "dlnl1 ~"
+            for regressors in specification[key]:
+                formula = formula + f" + {regressors}"
+            
+            formula = formula + "+ C(year) -1" #fixed effects
+
+            def cluster_fit(formula, data, group_var): #function for clustering with missing values
+                fit = smf.ols(formula, data=data).fit()
+                to_keep = pd.RangeIndex(len(data)).difference(pd.Index(fit.model.data.missing_row_idx))
+                robust = fit.get_robustcov_results(cov_type='cluster',
+                                                   groups=data.iloc[to_keep][group_var])
+                return robust #due to cluster fit the result is not a wrapper
+            
+            #c(var) for fixed effect - "-1" for dropping intercept
+            result = cluster_fit(formula, data=data, group_var="agidison")
+            
+            for index,coef in enumerate(specification[key]): #need to index by position since result isn't a wrapper
+                #find numer of fixed effect variables 
+                alpha = result.params.size - np.size(specification[key])
+                #find position of the coefficients
+                position = alpha + index #no need to set to zero by -1 sinze lengths are being used
+                
+                outputs = [result.params[position], result.bse[position], result.pvalues[position]]
+                table.loc[coef] = outputs
+
+        
+        container = pd.concat([container, table], axis=1)
+
+
+    
+    container.columns = pd.MultiIndex.from_product(
+            [specification.keys(),
+            ['Urbanization rate', 'Std.err', 'P-Value',]]
+            )
+    container = container.style.format('{:.3f}',na_rep='')
+
     return container
